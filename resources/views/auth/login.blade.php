15d3cc8fbc7bc88d01cdc712f8d16d645bed2513
fatima774 <fatimabougrin146@gmail.com>
2026-03-03 10:34:59 +0100

    Amélioration de la vue login : ajout des directives @error pour email et password

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,24 +7,25 @@
 <body>
     <h1>Connexion</h1>
 
-    @if ($errors->any())
-        <div style="color:red;">
-            @foreach ($errors->all() as $error)
-                <p>{{ $error }}</p>
-            @endforeach
-        </div>
-    @endif
-
     <form method="POST" action="/login">
         @csrf
 
         <label>Email :</label>
         <input type="email" name="email" required>
 
+    //ajout des directives d'affichage des erreurs de validation
+        @error('email') 
+        <p style="color:red;">{{ $message }}</p>
+         @enderror
+
         <br><br>
 
         <label>Mot de passe :</label>
         <input type="password" name="password" required>
+
+        @error('password')
+         <p style="color:red;">{{ $message }}</p>
+          @enderror
 
         <br><br>
 
