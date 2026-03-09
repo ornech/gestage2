a884a92d88bbf76241f8190eb2b8c9c31617b8c9
fatima774 <fatimabougrin146@gmail.com>
2026-03-09 09:42:02 +0100

    Ajout des modèles Employe/Stage + migrations propres

---
+++ database/migrations/2026_03_09_083253_create_stages_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stages', function (Blueprint $table) {
+            $table->id();
+             $table->string('titre');
+            $table->text('description')->nullable();
+            $table->date('date_debut');
+            $table->date('date_fin');
+            $table->foreignId('employe_id')->constrained('employes')->onDelete('cascade');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stages');
+    }
+};
