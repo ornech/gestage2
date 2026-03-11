900de4ccb63b0b09e5edcb4a812d2743e6916ac7
fatima774 <fatimabougrin146@gmail.com>
2026-03-11 09:21:16 +0100

    extraction de la validation vers des Form Requests (Stage & Employe)

--- app/Http/Requests/StoreEmployeRequest.php
+++ app/Http/Requests/StoreEmployeRequest.php
@@ -9,20 +9,19 @@ class StoreEmployeRequest extends FormRequest
     /**
      * Determine if the user is authorized to make this request.
      */
-    public function authorize(): bool
-    {
-        return false;
-    }
+public function authorize(): bool
+{
+    return true;
+}
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
-    {
-        return [
-            //
-        ];
-    }
+public function rules(): array
+{
+    return [
+        'nom' => 'required|string|max:255',
+        'prenom' => 'required|string|max:255',
+        'email' => 'required|email|unique:employes,email',
+        'entreprise_id' => 'required|exists:entreprises,id',
+    ];
+}
+
 }
--- app/Http/Requests/StoreStageRequest.php
+++ app/Http/Requests/StoreStageRequest.php
@@ -9,20 +9,19 @@ class StoreStageRequest extends FormRequest
     /**
      * Determine if the user is authorized to make this request.
      */
-    public function authorize(): bool
-    {
-        return false;
-    }
+   public function authorize(): bool
+{
+    return true;
+}
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
-    {
-        return [
-            //
-        ];
-    }
+public function rules(): array
+{
+    return [
+        'date_debut' => 'required|date',
+        'entreprise_id' => 'required|exists:entreprises,id',
+        'maitre_de_stage_id' => 'required|exists:employes,id',
+    ];
+}
+
+
 }
