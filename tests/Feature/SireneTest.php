e432e719db259a1fc12ddc9ba294a05b6af5cdd2
fatima774 <fatimabougrin146@gmail.com>
2026-03-17 14:23:36 +0100

    ajout de la structure initiale du test SireneTest
    -Création du test avec la commande : php artisan make:test SireneTest
    - Suppression du test par défaut généré par Laravel
    - Ajout des deux méthodes de test attendues par l’issue #106 :
      • it_returns_company_data_on_valid_siret()
      • it_handles_not_found_siret()

---
+++ tests/Feature/SireneTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class SireneTest extends TestCase
+{
+    /** @test */
+    public function it_returns_company_data_on_valid_siret()
+    {
+        //
+    }
+      /** @test */
+    public function it_handles_not_found_siret()
+    {
+        //
+    }
+}
