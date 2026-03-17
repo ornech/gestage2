59e52ee66fb65c4cb39707a94944255d23a6c3b8
fatima774 <fatimabougrin146@gmail.com>
2026-03-17 15:00:53 +0100

    ajout des tests 200 et 404 pour le service SireneClient
    - Ajout du test it_returns_company_data_on_valid_siret()
      • Simulation d'une réponse 200 via Http::fake()
      • Vérification que le service retournera les données de l'entreprise
    
    - Ajout du test it_handles_not_found_siret()
      • Simulation d'une réponse 404 via Http::fake()
      • Vérification que le service renverra null pour un SIRET introuvable

--- tests/Feature/SireneTest.php
+++ tests/Feature/SireneTest.php
@@ -11,11 +11,38 @@ class SireneTest extends TestCase
     /** @test */
     public function it_returns_company_data_on_valid_siret()
     {
-        //
+        // 1) On simule la réponse de l'API INSEE
+    Http::fake([
+        'https://api.insee.fr/*' => Http::response([
+            'etablissement' => [
+                'siret' => '12345678901234',
+                'uniteLegale' => [
+                    'denominationUniteLegale' => 'Entreprise Test'
+                ]
+            ]
+        ], 200)
+    ]);
+
+    // 2) On appelle notre futur service (il n'existe pas encore)
+    $client = new \App\Services\SireneClient();
+    $data = $client->getBySiret('12345678901234');
+
+    // 3) On vérifie que les données sont bien retournées
+    $this->assertEquals('Entreprise Test', $data['etablissement']['uniteLegale']['denominationUniteLegale']);
     }
       /** @test */
     public function it_handles_not_found_siret()
     {
-        //
+          // 1) On simule une réponse 404 de l'API INSEE
+    Http::fake([
+        'https://api.insee.fr/*' => Http::response([], 404)
+    ]);
+
+    // 2) On appelle notre futur service
+    $client = new \App\Services\SireneClient();
+    $data = $client->getBySiret('00000000000000');
+
+    // 3) On vérifie que le service renvoie null
+    $this->assertNull($data);
     }
 }
