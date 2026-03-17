63a4bbff87ee1f3c5a38610f3be0c403a08b02dc
fatima774 <fatimabougrin146@gmail.com>
2026-03-17 15:14:15 +0100

    ajout du service SireneClient et correction des imports dans SireneTest
    - Ajout du dossier app/Services et création de la classe SireneClient
    - Implémentation de la méthode getBySiret() avec appel API mockable
    - Ajout de l'import manquant Illuminate\Support\Facades\Http dans SireneTest
    - Les tests 200 et 404 passent désormais correctement

--- tests/Feature/SireneTest.php
+++ tests/Feature/SireneTest.php
@@ -5,7 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-
+use Illuminate\Support\Facades\Http;
 class SireneTest extends TestCase
 {
     /** @test */
