dab6cfbb0ec7984de4bfd7f8d0b4552000336614
fatima774 <fatimabougrin146@gmail.com>
2026-03-19 14:32:19 +0100

    ajout de la structure minimale des vues Contacts

---
+++ resources/views/contacts/create.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>Créer un contact</h1>
+
+
+<form method="POST" action="#">
+    @csrf
+</form>
+@endsection
---
+++ resources/views/contacts/show.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>Détails du contact</h1>
+
+
+@endsection
