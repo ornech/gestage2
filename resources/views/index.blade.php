0739d6518ee54c918569588a2d37b436eeab0cd5
fatima774 <fatimabougrin146@gmail.com>
2026-03-20 09:33:14 +0100

    Création des vues index et show entreprises

---
+++ resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1 class="title is-4">Annuaire des entreprises</h1>
+</div>
+@endsection
---
+++ resources/views/show.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1 class="title is-4">Fiche entreprise</h1>
+</div>
+@endsection
